d4ea1c0ddbb363ed75205c36001f0d62a37d601e
vilasha <mariavya@gmail.com>
2018-08-12 03:13:12 +0100

    task 26	MaxSliceSum (Task Score 100%, Correctness 100%, Performance 100%) from "Lesson 9 Maximum slice problem"

--- task26.java
+++ task26.java
@@ -1,47 +1,47 @@
-/*	MaxProfit
-	Given a log of stock prices compute the maximum possible earning.
+/*	MaxSliceSum
+	Find a maximum sum of a compact subsequence of array elements.
 	Task Score 100%, Correctness 100%, Performance 100%
 
-	An array A consisting of N integers is given. It contains daily prices of a stock share for a period of N consecutive days. If a single share was bought on day P and sold on day Q, where 0 ≤ P ≤ Q < N, then the profit of such transaction is equal to A[Q] − A[P], provided that A[Q] ≥ A[P]. Otherwise, the transaction brings loss of A[P] − A[Q].
+	A non-empty array A consisting of N integers is given. A pair of integers (P, Q), such that 0 ≤ P ≤ Q < N, is called a slice of array A. The sum of a slice (P, Q) is the total of A[P] + A[P+1] + ... + A[Q].
 
-	For example, consider the following array A consisting of six elements such that:
-	  A[0] = 23171
-	  A[1] = 21011
-	  A[2] = 21123
-	  A[3] = 21366
-	  A[4] = 21013
-	  A[5] = 21367
-	If a share was bought on day 0 and sold on day 2, a loss of 2048 would occur because A[2] − A[0] = 21123 − 23171 = −2048. If a share was bought on day 4 and sold on day 5, a profit of 354 would occur because A[5] − A[4] = 21367 − 21013 = 354. Maximum possible profit was 356. It would occur if a share was bought on day 1 and sold on day 5.
-
-	Write a function,
+	Write a function:
 	class Solution { public int solution(int[] A); }
-	that, given an array A consisting of N integers containing daily prices of a stock share for a period of N consecutive days, returns the maximum possible profit from one transaction during this period. The function should return 0 if it was impossible to gain any profit.
+	that, given an array A consisting of N integers, returns the maximum sum of any slice of A.
+
+	For example, given array A such that:
+	A[0] = 3  A[1] = 2  A[2] = -6
+	A[3] = 4  A[4] = 0
+	the function should return 5 because:
+	(3, 4) is a slice of A that has sum 4,
+	(2, 2) is a slice of A that has sum −6,
+	(0, 1) is a slice of A that has sum 5,
+	no other slice of A has sum greater than (0, 1).
 
 	Assume that:
-	N is an integer within the range [0..400,000];
-	each element of array A is an integer within the range [0..200,000].
+	N is an integer within the range [1..1,000,000];
+	each element of array A is an integer within the range [−1,000,000..1,000,000];
+	the result will be an integer within the range [−2,147,483,648..2,147,483,647].
 
 	Complexity:
 	expected worst-case time complexity is O(N);
-	expected worst-case space complexity is O(1) (not counting the storage required for input arguments).	*/
+	expected worst-case space complexity is O(N) (not counting the storage required for input arguments).	*/
 	
 class Solution {
-    /*  If it is only one or less prices in the array, we can't do
-        buy&sell transaction, i.e. we can't make any profit.
-        For larger arrays of prices we calculate max profit
-        if we buy at a minimum price and sell at current price.
-        Then we check if the current price is minimum. And the
-        resulting max profit will be either current profit or
-        previous max profit we found before */
+    /*  For each position we compute the largest sum, that
+        ends at this position. If this sum is less than
+        just the current element on its own, we take only
+        current element, so all the following sums will start
+        from this position. Otherwise we take previously
+        calculated sum plus the current element.
+        We add 1000001 to start position in case first element
+        is negative, so we don't get int overflow   */
     public int solution(int[] A) {
-        if (A.length <= 1)
-            return 0;
-        int maxCurrent = 0, maxProfit = 0, minPrice = A[0];
-        for (int i : A) {
-            maxCurrent = Math.max(0, i - minPrice);
-            minPrice = Math.min(minPrice, i);
-            maxProfit = Math.max(maxProfit, maxCurrent);
+        int currentSum = Integer.MIN_VALUE + 1000001;
+        int maxSum = Integer.MIN_VALUE + 1000001;
+        for (int a : A) {
+            currentSum = Math.max(currentSum + a, a);
+            maxSum = Math.max(maxSum, currentSum);
         }
-        return maxProfit;
+        return maxSum;
     }
 }
